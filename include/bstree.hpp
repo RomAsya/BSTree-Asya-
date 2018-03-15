db74031248428c1fa73e901d0e0ba8588af01313
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-03-15 20:42:36 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -6,12 +6,12 @@ struct Node {
 };
 class Tree {
   Node* root;
+  auto print_elements(Node* curr, int space) const -> void;
+  auto deleting(Node*&curr) -> void;
  public:
   Tree();
-  auto insert(int value) -> bool;
-  auto print_elements(Node* curr, int space) const -> void;
+  auto insert(int value) -> bool; 
   auto print() const -> void;
-  auto deleting(Node*&curr) -> void;
   ~Tree();
 };
 }
