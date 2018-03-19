5f5b28d38179d7c88cb8adb9a8ae7495d1f1afa2
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-03-19 21:45:10 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -8,10 +8,18 @@ class Tree {
   Node* root;
   auto print_elements(Node* curr, int space) const -> void;
   auto deleting(Node*&curr) -> void;
+  auto straight_detour(Node *curr) const -> void;
+  auto back_detour(Node *curr) const -> void;
+  auto transverse_detour(Node *curr) const -> void;
+
  public:
   Tree();
-  auto insert(int value) -> bool; 
+  auto insert(int value) -> bool;
   auto print() const -> void;
+  auto straight() const -> void;
+  auto back() const -> void;
+  auto transverse() const -> void;
+  auto get_tree_existence() const -> bool;
   ~Tree();
 };
 }
