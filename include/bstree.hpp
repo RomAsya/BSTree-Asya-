1e55b412e62c625e4fdcaf4a3351642511290df9
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-04-12 20:07:33 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -1,28 +1,33 @@
+#include <fstream>
+#include <string>
+#include <iostream>
 namespace BSTree {
-struct Node {
-  int data;
-  Node* left;
-  Node* right;
-  Node(int val);
-};
-class Tree {
-  Node* root;
-  auto print_elements(Node* curr, int space) const -> void;
-  auto deleting(Node*&curr) -> void;
-  auto straight_detour(Node *curr) const -> void;
-  auto back_detour(Node *curr) const -> void;
-  auto transverse_detour(Node *curr) const -> void;
+    struct Node {
+        int data;
+        Node* left;
+        Node* right;
+        Node(int val);
+    };
+    class Tree {
+        Node* root;
+        auto print_elements(Node* curr, int space) const -> void;
+        auto deleting(Node*&curr) -> void;
+        auto straight_detour(Node *curr) const -> void;
+        auto back_detour(Node *curr) const -> void;
+        auto transverse_detour(Node *curr) const -> void;
+        auto save_tree_to_the_file_recursion (Node * curr, int  space, std::ostream &File) ->void;
 
- public:
-  Tree();
-  auto insert(int value) -> bool;
-  auto print() const -> void;
-  auto straight() const -> void;
-  auto back() const -> void;
-  auto transverse() const -> void;
-  auto empty() const -> bool;
-  auto add_node(int value) -> void;
-  auto delete_node(int value) -> bool;
-  ~Tree();
-};
+        public:
+        Tree();
+        auto insert(int value) -> bool;
+        auto print() const -> void;
+        auto straight() const -> void;
+        auto back() const -> void;
+        auto transverse() const -> void;
+        auto empty() const -> bool;
+        auto add_node (int value) -> void;
+        auto delete_node (int value) -> bool;
+        auto save_tree_to_the_file (std::string file_name) -> bool;
+        ~Tree();
+    };
 }
